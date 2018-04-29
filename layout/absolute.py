0fa06a38099b2d97c9579df1a7379d593446d1d4
Jan Bodnar <vronskij@gmail.com>
2018-04-29 14:06:25 +0200

    first layout examples

---
+++ layout/absolute.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+ZetCode wxPython tutorial
+
+In this example, we lay out widgets using
+absolute positioning.
+
+author: Jan Bodnar
+website: www.zetcode.com
+last modified: April 2018
+"""
+
+import wx
+
+
+class Example(wx.Frame):
+
+    def __init__(self, parent, title):
+        super(Example, self).__init__(parent, title=title,
+            size=(350, 300))
+
+        self.InitUI()
+        self.Centre()
+
+    def InitUI(self):
+
+        self.panel = wx.Panel(self)
+
+        self.panel.SetBackgroundColour("gray")
+
+        self.LoadImages()
+
+        self.mincol.SetPosition((20, 20))
+        self.bardejov.SetPosition((40, 160))
+        self.rotunda.SetPosition((170, 50))
+
+
+    def LoadImages(self):
+
+        self.mincol = wx.StaticBitmap(self.panel, wx.ID_ANY,
+            wx.Bitmap("mincol.jpg", wx.BITMAP_TYPE_ANY))
+
+        self.bardejov = wx.StaticBitmap(self.panel, wx.ID_ANY,
+            wx.Bitmap("bardejov.jpg", wx.BITMAP_TYPE_ANY))
+
+        self.rotunda = wx.StaticBitmap(self.panel, wx.ID_ANY,
+            wx.Bitmap("rotunda.jpg", wx.BITMAP_TYPE_ANY))
+
+
+def main():
+
+    app = wx.App()
+    ex = Example(None, title='Absolute positioning')
+    ex.Show()
+    app.MainLoop()
+
+
+if __name__ == '__main__':
+    main()
