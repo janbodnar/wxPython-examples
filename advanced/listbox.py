9f0627c0ae7c1f55e17c932dae9297fa2d6fae09
Jan Bodnar <vronskij@gmail.com>
2018-05-14 18:13:31 +0200

    added code

--- advanced/listbox.py
+++ advanced/listbox.py
@@ -1,24 +1,93 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# centering.py
+"""
+ZetCode wxPython tutorial
+
+In this example, we create a wx.ListBox widget.
+
+author: Jan Bodnar
+website: www.zetcode.com
+last modified: May 2018
+"""
 
 import wx
 
 class Example(wx.Frame):
 
-    def __init__(self, parent, title):
-        super(Example, self).__init__(parent, title=title,
-            size=(300, 200))
+    def __init__(self, *args, **kw):
+        super(Example, self).__init__(*args, **kw)
 
+        self.InitUI()
+
+    def InitUI(self):
+
+        panel = wx.Panel(self)
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
+
+        self.listbox = wx.ListBox(panel)
+        hbox.Add(self.listbox, wx.ID_ANY, wx.EXPAND | wx.ALL, 20)
+
+        btnPanel = wx.Panel(panel)
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        newBtn = wx.Button(btnPanel, wx.ID_ANY, 'New', size=(90, 30))
+        renBtn = wx.Button(btnPanel, wx.ID_ANY, 'Rename', size=(90, 30))
+        delBtn = wx.Button(btnPanel, wx.ID_ANY, 'Delete', size=(90, 30))
+        clrBtn = wx.Button(btnPanel, wx.ID_ANY, 'Clear', size=(90, 30))
+
+        self.Bind(wx.EVT_BUTTON, self.NewItem, id=newBtn.GetId())
+        self.Bind(wx.EVT_BUTTON, self.OnRename, id=renBtn.GetId())
+        self.Bind(wx.EVT_BUTTON, self.OnDelete, id=delBtn.GetId())
+        self.Bind(wx.EVT_BUTTON, self.OnClear, id=clrBtn.GetId())
+        self.Bind(wx.EVT_LISTBOX_DCLICK, self.OnRename)
+
+        vbox.Add((-1, 20))
+        vbox.Add(newBtn)
+        vbox.Add(renBtn, 0, wx.TOP, 5)
+        vbox.Add(delBtn, 0, wx.TOP, 5)
+        vbox.Add(clrBtn, 0, wx.TOP, 5)
+
+        btnPanel.SetSizer(vbox)
+        hbox.Add(btnPanel, 0.6, wx.EXPAND | wx.RIGHT, 20)
+        panel.SetSizer(hbox)
+
+        self.SetTitle('wx.ListBox')
         self.Centre()
+
+    def NewItem(self, event):
+
+        text = wx.GetTextFromUser('Enter a new item', 'Insert dialog')
+        if text != '':
+            self.listbox.Append(text)
+
+    def OnRename(self, event):
+
+        sel = self.listbox.GetSelection()
+        text = self.listbox.GetString(sel)
+        renamed = wx.GetTextFromUser('Rename item', 'Rename dialog', text)
+
+        if renamed != '':
+            self.listbox.Delete(sel)
+            item_id = self.listbox.Insert(renamed, sel)
+            self.listbox.SetSelection(item_id)
+
+    def OnDelete(self, event):
+
+        sel = self.listbox.GetSelection()
+        if sel != -1:
+            self.listbox.Delete(sel)
+
+    def OnClear(self, event):
+        self.listbox.Clear()
+
 
 def main():
 
     app = wx.App()
-    ex = Example(None, title='Centering')
+    ex = Example(None)
     ex.Show()
     app.MainLoop()
+
 
 if __name__ == '__main__':
     main()
