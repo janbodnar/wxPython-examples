ae74aba716b00e2c7dfb22c6caaaccc93ab86df7
Jan Bodnar <vronskij@gmail.com>
2018-05-15 12:23:58 +0200

    new

---
+++ advanced/autowidth.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+ZetCode wxPython tutorial
+
+In this example, we use wx.lib.mixins.listctrl.ListCtrlAutoWidthMixin
+with a wx.ListBox.
+
+author: Jan Bodnar
+website: www.zetcode.com
+last modified: May 2018
+"""
+
+import wx
+import wx.lib.mixins.listctrl
+
+data = [('Jessica Alba', 'Pomona', '1981'), ('Sigourney Weaver', 'New York', '1949'),
+   ('Angelina Jolie', 'los angeles', '1975'), ('Natalie Portman', 'Jerusalem', '1981'),
+   ('Rachel Weiss', 'London', '1971'), ('Scarlett Johansson', 'New York', '1984')]
+
+
+class AutoWidthListCtrl(wx.ListCtrl, wx.lib.mixins.listctrl.ListCtrlAutoWidthMixin):
+
+    def __init__(self, parent, *args, **kw):
+        wx.ListCtrl.__init__(self, parent, wx.ID_ANY, style=wx.LC_REPORT)
+        wx.lib.mixins.listctrl.ListCtrlAutoWidthMixin.__init__(self)
+
+
+class Example(wx.Frame):
+
+    def __init__(self, *args, **kw):
+        super(Example, self).__init__(*args, **kw)
+
+        self.InitUI()
+
+    def InitUI(self):
+
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
+
+        panel = wx.Panel(self)
+
+        self.list = AutoWidthListCtrl(panel)
+        self.list.InsertColumn(0, 'name', width=140)
+        self.list.InsertColumn(1, 'place', width=130)
+        self.list.InsertColumn(2, 'year', wx.LIST_FORMAT_RIGHT, 90)
+
+        idx = 0
+
+        for i in data:
+
+            index = self.list.InsertItem(idx, i[0])
+            self.list.SetItem(index, 1, i[1])
+            self.list.SetItem(index, 2, i[2])
+            idx += 1
+
+        hbox.Add(self.list, 1, wx.EXPAND)
+        panel.SetSizer(hbox)
+
+        self.SetTitle('Actresses')
+        self.Centre()
+
+
+def main():
+
+    app = wx.App()
+    ex = Example(None)
+    ex.Show()
+    app.MainLoop()
+
+
+if __name__ == '__main__':
+    main()
