5899b7327620aba83c85246fda6f872c314b2e2e
Jan Bodnar <vronskij@gmail.com>
2018-05-15 10:59:03 +0200

    add

---
+++ advanced/actresses.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+ZetCode wxPython tutorial
+
+In this example, we create a simple
+wx.ListCtrl widget.
+
+author: Jan Bodnar
+website: www.zetcode.com
+last modified: May 2018
+"""
+
+import wx
+
+data = [('Jessica Alba', 'Pomona', '1981'), ('Sigourney Weaver', 'New York', '1949'),
+   ('Angelina Jolie', 'los angeles', '1975'), ('Natalie Portman', 'Jerusalem', '1981'),
+   ('Rachel Weiss', 'London', '1971'), ('Scarlett Johansson', 'New York', '1984' )]
+
+
+class Example(wx.Frame):
+
+    def __init__(self, *args, **kw):
+        super(Example, self).__init__(*args, **kw)
+
+        self.InitUI()
+
+    def InitUI(self):
+
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
+        panel = wx.Panel(self)
+
+        self.list = wx.ListCtrl(panel, wx.ID_ANY, style=wx.LC_REPORT)
+        self.list.InsertColumn(0, 'name', width=140)
+        self.list.InsertColumn(1, 'place', width=130)
+        self.list.InsertColumn(2, 'year', wx.LIST_FORMAT_RIGHT, 90)
+
+        idx = 0
+
+        for i in data:
+
+            index = self.list.InsertItem(idx, i[0])
+            self.list.SetItem(index, 1, i[1])
+            self.list.SetItem(index, 2, i[2])
+            idx += 1
+
+        hbox.Add(self.list, 1, wx.EXPAND)
+        panel.SetSizer(hbox)
+
+        self.SetTitle('Actresses')
+        self.Centre()
+
+
+def main():
+
+    app = wx.App()
+    ex = Example(None)
+    ex.Show()
+    app.MainLoop()
+
+
+if __name__ == '__main__':
+    main()
