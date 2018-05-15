77e56895b19d86db390df0b08c11fe2cc790e10b
Jan Bodnar <vronskij@gmail.com>
2018-05-15 12:47:00 +0200

    new

---
+++ advanced/repository.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+ZetCode wxPython tutorial
+
+In this example, we create a wx.ListBox widget.
+
+author: Jan Bodnar
+website: www.zetcode.com
+last modified: May 2018
+"""
+
+import wx
+from wx.lib.mixins.listctrl import CheckListCtrlMixin, ListCtrlAutoWidthMixin
+
+packages = [('abiword', '5.8M', 'base'), ('adie', '145k', 'base'),
+    ('airsnort', '71k', 'base'), ('ara', '717k', 'base'), ('arc', '139k', 'base'),
+    ('asc', '5.8M', 'base'), ('ascii', '74k', 'base'), ('ash', '74k', 'base')]
+
+class CheckListCtrl(wx.ListCtrl, CheckListCtrlMixin, ListCtrlAutoWidthMixin):
+
+    def __init__(self, parent):
+        wx.ListCtrl.__init__(self, parent, wx.ID_ANY, style=wx.LC_REPORT |
+                wx.SUNKEN_BORDER)
+        CheckListCtrlMixin.__init__(self)
+        ListCtrlAutoWidthMixin.__init__(self)
+
+
+class Example(wx.Frame):
+
+    def __init__(self, *args, **kw):
+        super(Example, self).__init__(*args, **kw)
+
+        panel = wx.Panel(self)
+
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
+
+        leftPanel = wx.Panel(panel)
+        rightPanel = wx.Panel(panel)
+
+        self.log = wx.TextCtrl(rightPanel, style=wx.TE_MULTILINE|wx.TE_READONLY)
+        self.list = CheckListCtrl(rightPanel)
+        self.list.InsertColumn(0, 'Package', width=140)
+        self.list.InsertColumn(1, 'Size')
+        self.list.InsertColumn(2, 'Repository')
+
+        idx = 0
+
+        for i in packages:
+
+            index = self.list.InsertItem(idx, i[0])
+            self.list.SetItem(index, 1, i[1])
+            self.list.SetItem(index, 2, i[2])
+            idx += 1
+
+        vbox2 = wx.BoxSizer(wx.VERTICAL)
+
+        selBtn = wx.Button(leftPanel, label='Select All')
+        desBtn = wx.Button(leftPanel, label='Deselect All')
+        appBtn = wx.Button(leftPanel, label='Apply')
+
+        self.Bind(wx.EVT_BUTTON, self.OnSelectAll, id=selBtn.GetId())
+        self.Bind(wx.EVT_BUTTON, self.OnDeselectAll, id=desBtn.GetId())
+        self.Bind(wx.EVT_BUTTON, self.OnApply, id=appBtn.GetId())
+
+        vbox2.Add(selBtn, 0, wx.TOP|wx.BOTTOM, 5)
+        vbox2.Add(desBtn, 0, wx.BOTTOM, 5)
+        vbox2.Add(appBtn)
+
+        leftPanel.SetSizer(vbox2)
+
+        vbox.Add(self.list, 4, wx.EXPAND | wx.TOP, 3)
+        vbox.Add((-1, 10))
+        vbox.Add(self.log, 1, wx.EXPAND)
+        vbox.Add((-1, 10))
+
+        rightPanel.SetSizer(vbox)
+
+        hbox.Add(leftPanel, 0, wx.EXPAND | wx.RIGHT, 5)
+        hbox.Add(rightPanel, 1, wx.EXPAND)
+        hbox.Add((3, -1))
+
+        panel.SetSizer(hbox)
+
+        self.SetTitle('Repository')
+        self.Centre()
+
+    def OnSelectAll(self, event):
+
+        num = self.list.GetItemCount()
+        for i in range(num):
+            self.list.CheckItem(i)
+
+    def OnDeselectAll(self, event):
+
+        num = self.list.GetItemCount()
+        for i in range(num):
+            self.list.CheckItem(i, False)
+
+    def OnApply(self, event):
+
+        num = self.list.GetItemCount()
+
+        for i in range(num):
+
+            if i == 0: self.log.Clear()
+
+            if self.list.IsChecked(i):
+                self.log.AppendText(self.list.GetItemText(i) + '\n')
+
+
+def main():
+
+    app = wx.App()
+    ex = Example(None)
+    ex.Show()
+    app.MainLoop()
+
+
+if __name__ == '__main__':
+    main()
