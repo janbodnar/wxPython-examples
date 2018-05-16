21cd28adc95a9950064fe781f8765bd48b99ab3f
Jan Bodnar <vronskij@gmail.com>
2018-05-16 09:17:07 +0200

    update

--- graphics/draw_line.py
+++ graphics/draw_line.py
@@ -9,28 +9,37 @@ frame window after a while.
 
 author: Jan Bodnar
 website: zetcode.com
-last edited: April 28 2018
+last edited: May 2018
 """
 
 import wx
 
 class Example(wx.Frame):
 
-    def __init__(self, parent, title):
-        super(Example, self).__init__(parent, title=title,
-            size=(350, 250))
+    def __init__(self, *args, **kw):
+        super(Example, self).__init__(*args, **kw)
+
+        self.InitUI()
+
+    def InitUI(self):
 
         wx.CallLater(2000, self.DrawLine)
 
+        self.SetTitle("Line")
         self.Centre()
 
     def DrawLine(self):
+
         dc = wx.ClientDC(self)
         dc.DrawLine(50, 60, 190, 60)
 
-if __name__ == '__main__':
+def main():
 
     app = wx.App()
-    ex = Example(None, 'Line')
+    ex = Example(None)
     ex.Show()
     app.MainLoop()
+
+
+if __name__ == '__main__':
+    main()
