c6d5ddc8917f8cd4ca6980471cb058e75d4162d9
Jan Bodnar <vronskij@gmail.com>
2018-05-16 10:49:49 +0200

    new

---
+++ graphics/custom_patterns.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+ZetCode wxPython tutorial
+
+This program draws three rectangles with custom
+brush patterns.
+
+author: Jan Bodnar
+website: zetcode.com
+last edited: May 2018
+"""
+
+import wx
+
+class Example(wx.Frame):
+
+    def __init__(self, *args, **kw):
+        super(Example, self).__init__(*args, **kw)
+
+        self.InitUI()
+
+    def InitUI(self):
+
+        self.Bind(wx.EVT_PAINT, self.OnPaint)
+
+        self.SetTitle("Custom patterns")
+        self.Centre()
+
+    def OnPaint(self, e):
+
+        dc = wx.PaintDC(self)
+
+        dc.SetPen(wx.Pen('#C7C3C3'))
+
+        brush1 = wx.Brush(wx.Bitmap('pattern1.png'))
+        dc.SetBrush(brush1)
+        dc.DrawRectangle(10, 15, 90, 60)
+
+        brush2 = wx.Brush(wx.Bitmap('pattern2.png'))
+        dc.SetBrush(brush2)
+        dc.DrawRectangle(130, 15, 90, 60)
+
+        brush3 = wx.Brush(wx.Bitmap('pattern3.png'))
+        dc.SetBrush(brush3)
+        dc.DrawRectangle(250, 15, 90, 60)
+
+
+def main():
+
+    app = wx.App()
+    ex = Example(None)
+    ex.Show()
+    app.MainLoop()
+
+
+if __name__ == '__main__':
+    main()
