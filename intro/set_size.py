1f45446793897ee9b6f56ffe8a073b7e7804f499
root <admin@zetcode.com>
2020-08-18 11:14:15 +0200

    update path and remove encoding

--- intro/set_size.py
+++ intro/set_size.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+#!/usr/bin/python
 
 # set_size.py
 
--- waiting.py
+++ waiting.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
 
 '''
 ZetCode Advanced wxPython tutorial
